4c11810ba257d2430a806290a9f5240d2a4e7e8e
Andres Zambrano <andreszb@gmail.com>
2025-06-16 20:43:14 -0600

    Fixed SSH control directory error

--- packages/openssh.nix
+++ packages/openssh.nix
@@ -10,4 +10,12 @@
     controlPath = "~/.ssh/control/%C";
     controlPersist = "10m";
   };
+
+  # Create control socket directory to prevent "cannot bind to path" errors
+  # when using SSH connection multiplexing (controlMaster = "auto")
+  fileConfig = {
+    ".ssh/control/.keep" = {
+      text = "";
+    };
+  };
 }
